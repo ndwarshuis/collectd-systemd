72d3a8e32c562273d55d84c52b7baad0783bbd92
petrucci4prez <natedwarshuis@gmail.com>
2020-05-22 11:20:56 -0400

    ADD second record for whether or not the service is active

--- collectd_systemd.py
+++ collectd_systemd.py
@@ -35,16 +35,22 @@ class SystemD(object):
             self.units[name] = unit
         return self.units[name]
 
-    def get_service_state(self, name):
+    def get_service_state(self, name, prop, failval):
         unit = self.get_unit(name)
         if not unit:
-            return 'broken'
+            return failval
         else:
             try:
-                return unit.Get('org.freedesktop.systemd1.Unit', 'SubState')
+                return unit.Get('org.freedesktop.systemd1.Unit', prop)
             except dbus.exceptions.DBusException as e:
                 self.log_verbose('{} plugin: failed to monitor unit {}: {}'.format(self.plugin_name, name, e))
-                return 'broken'
+                return failval
+
+    def get_service_substate(self, name):
+        return self.get_service_state(name, "SubState", "broken")
+
+    def get_service_activestate(self, name):
+        return self.get_service_state(name, "ActiveState", "failed")
 
     def configure_callback(self, conf):
         for node in conf.children:
@@ -71,22 +77,36 @@ class SystemD(object):
         for name in self.services:
             full_name = name + '.service'
 
-            state = self.get_service_state(full_name)
-            if state == 'broken':
+            substate = self.get_service_substate(full_name)
+            if substate == 'broken':
                 self.log_verbose ('Unit {0} reported as broken. Reinitializing the connection to dbus & retrying.'.format(full_name))
                 self.init_dbus()
-                state = self.get_service_state(full_name)
+                substate = self.get_service_substate(full_name)
 
-            value = (1.0 if state == 'running' or state == 'reload' else 0.0)
-            self.log_verbose('Sending value: {}.{}={} (state={})'
-                             .format(self.plugin_name, name, value, state))
-            val = collectd.Values(
+            # send substate value
+            subvalue = (1.0 if substate == 'running' or substate == 'reload' else 0.0)
+            self.log_verbose('Sending value: {}.{}={} (substate={})'
+                             .format(self.plugin_name, name, subvalue, substate))
+            subRecord = collectd.Values(
                 type='gauge',
                 plugin=self.plugin_name,
                 plugin_instance=name,
                 type_instance='running',
-                values=[value])
-            val.dispatch()
+                values=[subvalue])
+            subRecord.dispatch()
+
+            # send activestate value
+            activestate = self.get_service_activestate(full_name)
+            activevalue = (1.0 if activestate == 'active' else 0.0)
+            self.log_verbose('Sending value: {}.{}={} (activestate={})'
+                             .format(self.plugin_name, name, activevalue, activestate))
+            activeRecord = collectd.Values(
+                type='gauge',
+                plugin=self.plugin_name,
+                plugin_instance=name,
+                type_instance='active',
+                values=[activevalue])
+            activeRecord.dispatch()
 
 
 mon = SystemD()
